41ab477aab3421c6194da12c9725f10c1af5284e
kategerasimenko <katgerasimenko@gmail.com>
2021-01-05 19:28:06 +0300

    fix eaf parsing

--- trimco/corpora/utils/word_list.py
+++ trimco/corpora/utils/word_list.py
@@ -11,31 +11,54 @@ from .misc import (
 
 
 standartization_regex = re.compile(r'^(.+?)_standartization$')
-standartization_num_regex = re.compile(r'^\d+:')
-annotation_filtering_regex = re.compile(r'.+?:.+?:')
+standartization_num_regex = re.compile(r'^(\d+):(.+)')
+annotation_num_regex = re.compile(r'(\d+?):.+?:(.+)')
+
+
+def get_tier_alignment(orig_tier, standartization_tier, annotation_tier):
+    tier_alignment = {(ann[0], ann[1]): [ann[2], None, None] for ann in orig_tier}
+
+    for ann in standartization_tier:
+        if (ann[0], ann[1]) in tier_alignment:
+            tier_alignment[(ann[0], ann[1])][1] = ann[2]
+
+    for ann in annotation_tier:
+        if (ann[0], ann[1]) in tier_alignment:
+            tier_alignment[(ann[0], ann[1])][2] = ann[2]
+
+    return tier_alignment
 
 
 def process_one_tier(eaf_filename, words, orig_tier, standartization_tier, annotation_tier):
-    for orig, standartization, annotation in zip(orig_tier, standartization_tier, annotation_tier):
-        orig_words = clean_transcription(orig[2]).split()
-        standartizations = standartization[2].split(ANNOTATION_WORD_SEP)
-        annotations = annotation[2].split(ANNOTATION_WORD_SEP)
-        eq_length = len(orig_words) == len(standartizations) == len(annotations)
-        if not eq_length:
-            print(
-                'ERROR: ' + eaf_filename + ': mismatch between orig sentence and annotation.' +
-                ' Orig length', len(orig_words),
-                ' Standartization length', len(standartizations),
-                ' Annotation length', len(annotations)
-            )
+    tier_alignment = get_tier_alignment(orig_tier, standartization_tier, annotation_tier)
+    for orig, standartization, annotation in tier_alignment.values():
+        if standartization is None or annotation is None:
             continue
 
-        for word, std, anns in zip(orig_words, standartizations, annotations):
-            std = standartization_num_regex.sub('', std)
-            anns = annotation_filtering_regex.sub('', anns)
-            anns = anns.split(ANNOTATION_OPTION_SEP)
+        standartizations = {}
+        for std in standartization.split(ANNOTATION_WORD_SEP):
+            std_num, std = standartization_num_regex.search(std).groups()
+            standartizations[int(std_num)] = std
+
+        annotations = {}
+        for ann in annotation.split(ANNOTATION_WORD_SEP):
+            ann_num, ann = annotation_num_regex.search(ann).groups()
+            annotations[int(ann_num)] = ann
+
+        for i, word in enumerate(clean_transcription(orig).split()):
+            std = standartizations.get(i)
+            if std is None:
+                print(eaf_filename, 'no std for word ' + str(i), orig, standartization, '', sep='\n')
+                continue
 
             words['words'][word].append(std)
+
+            anns = annotations.get(i)
+            if anns is None:
+                print(eaf_filename, 'no ann for word ' + str(i), orig, annotation, '', sep='\n')
+                continue
+
+            anns = anns.split(ANNOTATION_OPTION_SEP)
             for ann in anns:
                 words['standartizations'][std].append(ann)
 
@@ -63,9 +86,13 @@ def process_one_elan(eaf_filename):
             continue
 
         speaker = standartization_tier_name.group(1)
-        orig_tier = sorted(eaf_obj.get_annotation_data_for_tier(speaker), key=lambda x: x[0])
-        standartization_tier = sorted(eaf_obj.get_annotation_data_for_tier(tier_name), key=lambda x: x[0])
-        annotation_tier = sorted(eaf_obj.get_annotation_data_for_tier(speaker + '_annotation'), key=lambda x: x[0])
+        try:
+            orig_tier = sorted(eaf_obj.get_annotation_data_for_tier(speaker), key=lambda x: x[0])
+            standartization_tier = sorted(eaf_obj.get_annotation_data_for_tier(tier_name), key=lambda x: x[0])
+            annotation_tier = sorted(eaf_obj.get_annotation_data_for_tier(speaker + '_annotation'), key=lambda x: x[0])
+        except KeyError:
+            print('ERROR: no tier, ' + eaf_filename)  # TODO: write log or solve problem
+            continue
 
         words = process_one_tier(eaf_filename, words, orig_tier, standartization_tier, annotation_tier)
 
