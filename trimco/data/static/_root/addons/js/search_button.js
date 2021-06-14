dd2ea2e1be63841029ea8b6565fbe9cb3cde67b0
kategerasimenko <katgerasimenko@gmail.com>
2021-06-14 20:26:17 +0300

    remove unnecessary ajax

--- trimco/data/static/_root/addons/js/search_button.js
+++ trimco/data/static/_root/addons/js/search_button.js
@@ -1,14 +1,9 @@
 (function($) {
     $(document).ready(function() {
-        var search_div = $('#search');
+        var search_div = $('.grp-change-list #search');
         if (search_div) {
             var search_url = document.URL + 'search';
-            $.ajax({
-                url: search_url,
-                success: function() {
-                    search_div.prepend('<a href="' + search_url + '" class="grp-button" id="search_button">Advanced search</a>');
-                }
-            });
+            search_div.prepend('<a href="' + search_url + '" class="grp-button" id="search_button">Advanced search</a>');
         };
     });
 })(grp.jQuery);
